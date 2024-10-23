e7441503c8dbb5469228839834c8483be63fed62
aimahbilal1 <233533@students.au.edu.pk>
2024-10-23 12:51:51 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,,] matrix = new int[3, 3, 3];
+        Console.WriteLine("Enter the values for the 3x3x3 Matrix : ");
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                for(int k = 0; k<3; k++)
+                {
+                    Console.Write($"Enter the value for Matrix[{i},{j},{k}]: ");
+                    matrix[i, j, k] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+        int diagonalsum = 0;
+        for (int i = 0;i<3; i++)
+        {
+            diagonalsum += matrix[i, i, i];
+        }
+        Console.WriteLine($"The sum of the diagonal element is: {diagonalsum}");
+    }
+}
